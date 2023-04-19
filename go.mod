cdbf4852852c61cd321e1ff8afbd9aa9e8cc1426
Tyler Mumford <tyler.mumford@icloud.com>
2023-04-19 15:13:05 -0600

    Add main.go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tylermumford/friendly-broccoli
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,9 @@
+// brock is a tool for automatically monitoring your local dev environment.
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello!")
+}
