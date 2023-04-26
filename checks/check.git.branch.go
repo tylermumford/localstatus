a4691627145a2e0e57af9967cd5d8b17fa3415f5
Tyler Mumford <tyler.mumford@icloud.com>
2023-04-25 23:53:04 -0600

    Add output, solves #1

--- checks/check.git.branch.go
+++ checks/check.git.branch.go
@@ -1,6 +1,7 @@
 package checks
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
 	"path"
@@ -25,6 +26,7 @@ type CheckGitBranch struct{}
 
 func (g CheckGitBranch) Run(p Params) CheckResult {
 	dir := p.GetString("dir")
+	dirEnd := path.Base(dir)
 	base := p.GetString("base")
 
 	_, err := os.Lstat(dir)
@@ -47,13 +49,14 @@ func (g CheckGitBranch) Run(p Params) CheckResult {
 
 	comparison, err := cmd.Output()
 	if err != nil {
-		return newBasicResult(false, "running git: "+err.Error())
+		msg := fmt.Sprintf("running git in %s: %s", dir, err.Error())
+		return newBasicResult(false, msg)
 	}
 
 	// TODO: It would be nice to count the commits.
 	if len(comparison) > 0 {
-		return newBasicResult(false, "New commits on "+base)
+		return newBasicResult(false, dirEnd+": New commits on "+base)
 	}
 
-	return newBasicResult(true, "Up to date with "+base)
+	return newBasicResult(true, dirEnd+": Up to date with "+base)
 }
